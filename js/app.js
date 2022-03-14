896689e11a952fd888ed83e2a295d94285fc84af
rich <rajeffords@gmail.com>
2022-03-14 13:55:00 -0400

    improvement(pizza): gimme pizzaaaaa

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-window.addEventListener("load", function () {
-  var bgs = ["pizza-trippy.gif", "trippy-pizza.gif", "tile-pizza.gif"]
+var bgs = ["pizza-trippy.gif", "trippy-pizza.gif", "tile-pizza.gif"]
+
+function setBg() {
   var bg = bgs[Math.floor(Math.random() * bgs.length)]
   document.body.style.backgroundImage = "url(static/img/" + bg + ")"
+}
+
+window.addEventListener("load", function () {
+  setBg()
+})
+
+var isAudioPlaying = false
+var audioEl = document.getElementById("gimme-pizza")
+document.addEventListener("click", function () {
+  if (isAudioPlaying) {
+    audioEl.pause()
+  }
+  else {
+    audioEl.play()
+  }
+  isAudioPlaying = !isAudioPlaying
+  setBg()
 })
