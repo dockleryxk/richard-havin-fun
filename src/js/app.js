43a828e3ac3fba0d1a525fec4ad59c3120dd6bc0
rich <rajeffords@gmail.com>
2022-03-15 23:08:33 -0400

    feat: add another easter egg

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,37 @@
 var bgClasses = ["pizza-trippy", "trippy-pizza", "tile-pizza"]
 var bgClass = ""
 var count = 0
+var found = false
+var isAudioPlaying = false
+var audioEl = document.getElementById("gimme-pizza")
 
 function setBg(removeClass) {
-  if (removeClass) {
-    document.body.classList.remove(bgClass)
+  if (++count < 10) {
+    if (removeClass) {
+      document.body.classList.remove(bgClass)
+    }
+    bgClass = bgClasses[Math.floor(Math.random() * bgClasses.length)]
+    document.body.classList.add(bgClass)
   }
-  bgClass = bgClasses[Math.floor(Math.random() * bgClasses.length)]
-  document.body.classList.add(bgClass)
+  else if (!found) {
+    found = true
+    document.body.classList.remove(bgClass)
+    document.body.classList.add("big-rich")
+    audioEl.play()
+  }
 }
 
 setBg(false)
 
-var isAudioPlaying = false
-var audioEl = document.getElementById("gimme-pizza")
 document.addEventListener("click", function () {
-  if (isAudioPlaying) {
-    audioEl.pause()
+  if (!found) {
+    if (isAudioPlaying) {
+      audioEl.pause()
+    }
+    else {
+      audioEl.play()
+    }
+    isAudioPlaying = !isAudioPlaying
+    setBg(true)
   }
-  else {
-    audioEl.play()
-  }
-  isAudioPlaying = !isAudioPlaying
-  setBg(true)
 })
