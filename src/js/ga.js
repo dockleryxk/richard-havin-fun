5eca6f2ce1a916570283fbeba43db7ae73c20ce5
rich <rajeffords@gmail.com>
2022-03-17 21:45:16 -0400

    fix: preloads

--- src/js/ga.js
+++ src/js/ga.js
@@ -1,10 +1,12 @@
 /* jshint ignore:start */
-(function (i, s, o, g, r, a, m) {
-  i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-    (i[r].q = i[r].q || []).push(arguments)
-  }, i[r].l = 1 * new Date(); a = s.createElement(o),
-    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
-ga('create', 'UA-76246852-1', 'auto')
-ga('send', 'pageview')
+if (navigator && navigator.userAgent && (navigator.userAgent.includes("Page Speed") || navigator.userAgent.includes("Chrome-Lighthouse"))) {
+  (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+  ga('create', 'UA-76246852-1', 'auto')
+  ga('send', 'pageview')
+}
 /* jshint ignore:end */
